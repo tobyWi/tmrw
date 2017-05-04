3fa43028a1e3d28d6ecc71c91517ac78a6da3a71
Tobias Wiklund <tobias.wiklund@gmail.com>
2017-05-04 15:44:35 +0200

    responsive menu now closes when clicking outside

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,12 @@ window.onload = function() {
     document.getElementById("defaultopen").click();
 
 
+    var menuToHide = document.getElementById('myTopnav');
+    document.onclick = function(e){
+        if(e.target.id !== 'bars') {
+            menuToHide.className = 'responsive-menu';
+        }
+    }    
 }
 
 
@@ -9,12 +15,15 @@ function openTab(evt, tabName) {
 
     var i, 
     tabcontent, 
-    tablinks;
+    tablinks,
+    responsivelinks;
+
+
+    var x = document.getElementById("myTopnav");
     
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
-
     }
 
     tablinks = document.getElementsByClassName("tab-link");
@@ -26,13 +35,15 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
-   function myFunction() {
+
+function myFunction() {
     var x = document.getElementById("myTopnav");
         if (x.className === "responsive-menu") {
             x.className += " show";
         } else {
             x.className = "responsive-menu";
-        }
-
+        } 
 }
+
+
 
