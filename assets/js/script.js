3cd861cf162de218dbaec111e999b0eee7af71cf
Tobias Wiklund <tobias.wiklund@gmail.com>
2017-05-04 14:08:19 +0200

    no commentx

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,6 @@ function openTab(evt, tabName) {
     var i, 
     tabcontent, 
     tablinks;
-
     
     tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
